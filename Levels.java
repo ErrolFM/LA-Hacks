b72483b9819cfae45b7aa1038fb636166b215ca8
ErrolFM <102879281+ErrolFM@users.noreply.github.com>
2022-04-17 00:43:46 -0700

    Create Levels.java

---
+++ Levels.java
@@ -0,0 +1,30 @@
+import java.util.scanner;
+public class Levels extends MainTimer{
+  private String choice;
+  public Levels(String choice){
+  this.choice = choice;
+  }
+  // 60 seconds is one minute & 30 minutes is 1800 seconds, 30 minute interval!
+  public 
+  if(choice.equals("Beginner")){
+    if(seconds = 1800){
+    System.out.println("You made it through the Beginner study session!");
+    System.out.println("Restart App?");
+    }
+  }
+  else if(choice.equals("Intermediate")){
+    if(seconds = 2700){
+    System.out.println("You made it through the Beginner study session!");
+    System.out.println("Restart App?");
+    }
+  }
+  else if(choice.equals("Determined")){
+    if(seconds = 3600){
+    System.out.println("You made it through the Beginner study session!");
+    System.out.println("Restart App?");
+    }
+  }
+  else{
+  break;
+  }
+}
