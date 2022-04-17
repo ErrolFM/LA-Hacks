86b1b49ffa6adb0c872c4a59f4fa5d10ba04fdd8
ErrolFM <102879281+ErrolFM@users.noreply.github.com>
2022-04-16 23:33:41 -0700

    Update MainTimer.java

--- MainTimer.java
+++ MainTimer.java
@@ -1,6 +1,13 @@
 public class MainTimer{
+  int seconds = 0;
   Countdown countdown = new Countdown();
+  CountdownTask task = new CountdownTask{
+    public void increment(){
+      seconds++;
+      System.out.println("Time Passed: " + seconds);
+    }
+  };
   public void begin(){
-    
+    myTimer.scheduleAtFixedRate(task, 1000, 1000);
   }
 }
